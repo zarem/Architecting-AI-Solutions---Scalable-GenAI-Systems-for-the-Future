f6b34d459466bfd4b6c650de8e91bc791efece21
zarem <zarem@uw.edu>
2026-09-05 18:33:14 -0700

    Update genai_full_simulation.py

--- genai_full_simulation.py
+++ genai_full_simulation.py
@@ -249,8 +249,8 @@ async def main():
 
 
 # Run the simulation
-#asyncio.run(main())
-await main()
+asyncio.run(main())
+
 
 # Print feedback data for debugging (Loop to improve model later)
 cursor.execute("SELECT * FROM feedback")
